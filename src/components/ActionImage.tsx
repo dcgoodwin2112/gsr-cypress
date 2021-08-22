1c677119adfeaf5b8ece2d805f1622ec868df7b3
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-08-22 19:53:15 -0400

    Add silly alt tags to cat images

--- src/components/ActionImage.tsx
+++ src/components/ActionImage.tsx
@@ -1,26 +1,35 @@
-import styled from "styled-components";
+import styled from "styled-components"
 import COLORS from "../styles/colors"
-import { ACTIONS } from "../lib/game";
+import { ACTIONS } from "../lib/game"
 
 type ActionImageProps = {
-  children: React.ReactNode;
-  action: ACTIONS;
-};
+  children: React.ReactNode
+  action: ACTIONS
+}
 
 export default function ActionImage({ children, action }: ActionImageProps) {
   return (
     <Wrapper>
       {action === ACTIONS.GOOD ? (
-        <ImageWrapper src="/good_cat.png" alt="Image of Rocks"/>
+        <ImageWrapper
+          src="/good_cat.png"
+          alt="Good cat is good kitty. Maybe not as good of a boy as his puppy friends, but compared to most cats this is one saintly feline"
+        />
       ) : action === ACTIONS.SAD ? (
-        <ImageWrapper src="/sad_cat.png" alt="Image of Paper" />
+        <ImageWrapper
+          src="/sad_cat.png"
+          alt="Sad cat is having a bad day. He tried to catch a mouse but lost it, his human picked up the wrong cat food, and he even had to get a bath. Don't worry sad cat. Tomorrow will be better."
+        />
       ) : action === ACTIONS.RAD ? (
-        <ImageWrapper src="/rad_cat.png" alt="Image of Scissors" />
+        <ImageWrapper
+          src="/rad_cat.png"
+          alt="Rad cat is just too rad. The world cannot handle his level of cool. Keep doing you rad cat. The world will catch up eventually."
+        />
       ) : (
         <PlaceholderWrapper>{children}</PlaceholderWrapper>
       )}
     </Wrapper>
-  );
+  )
 }
 
 const Wrapper = styled.div`
@@ -33,7 +42,7 @@ const Wrapper = styled.div`
 
 const ImageWrapper = styled.img`
   position: relative;
-`;
+`
 
 const PlaceholderWrapper = styled.div`
   display: flex;
